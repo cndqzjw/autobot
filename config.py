353908e0281a543a54f3a95442079c6fde0c896a
cndqzjw <cndqzjw@outlook.com>
2025-06-23 04:56:29 +0800

    first commit

--- config.py
+++ config.py
@@ -1,12 +1,3 @@
 config = {
-    'api_key': 'e178e19a-de8e-4651-b9c0-734619dd0b35',
-    'secret_key': '51868B474331987917E22F7F8E4D8A66',
-    'passphrase': 'DQzhoujw$#$kk73',
-    'symbol': 'SOL-USDT',
-    'base_url': 'https://www.okx.com',
-    'telegram_token': '7650712304:AAGi4GYCB3NI2ARhwUyih3Uw5ff4NxsxNcs',
-    'telegram_chat_id': '7841093729',
-    'risk_percent': 0.05,
-    'max_drawdown_ratio': 0.15,
-    'daily_profit_target': 0.05
+
 }
